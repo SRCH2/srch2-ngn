3f9b9ba96b378e0c5060c462a6ce63673ed1c667
jianfeng <jianfengjia@srch2.com>
2013-07-16 15:59:19 -0700

    remove release and debug

--- debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jianfeng/workspace/srch2-ngn-jianfeng
-# Build directory: /home/jianfeng/workspace/srch2-ngn-jianfeng/debug
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(include/instantsearch)
-SUBDIRS(src/core)
-SUBDIRS(test/core/lib4Android)
