dff254bad08b9d70ed08c431ff08101df9e14809
huaijie <huaijie@srch2.com>
2013-07-18 15:17:49 +0800

    polish the wrapper testing

--- test/wrapper/system_tests/main.sh
+++ test/wrapper/system_tests/main.sh
@@ -1,39 +1,39 @@
-cd exact_A1
+cd exact_a1
 echo '----do exact_A1 test--------------'
 python exact_A1.py queriesAndResults.txt
 
 cd ..
-cd fuzzy_A1
+cd fuzzy_a1
 echo '----do fuzzy_A1 test--------------'
-python fuzzy_A1.py queriesAndResults.txt
+python fuzzy_a1.py queriesAndResults.txt
 
 cd ..
-cd exact_M1
+cd exact_m1
 echo '----do exact_M1 test--------------'
 python exact_M1.py queriesAndResults.txt
 
 cd ..
-cd fuzzy_M1
+cd fuzzy_m1
 echo '----do fuzzy_M1 test--------------'
 python fuzzy_M1.py queriesAndResults.txt
 
 cd ..
-cd exact_Attribute_Based_Search
+cd exact_attribute_based_search
 echo '----do exact_Attribute_Based_Search test--------------'
 python exact_Attribute_Based_Search.py queriesAndResults.txt
 
 cd ..
-cd fuzzy_Attribute_Based_Search
+cd fuzzy_attribute_based_search
 echo '----do fuzzy_Attribute_Based_Search test--------------'
 python fuzzy_Attribute_Based_Search.py queriesAndResults.txt
 
 cd ..
-cd exact_Attribute_Based_Search_Geo
+cd exact_attribute_based_search_geo
 echo '----do exact_Attribute_Based_Search_Geo test--------------'
 python exact_Attribute_Based_Search_Geo.py queriesAndResults.txt
 
 cd ..
-cd fuzzy_Attribute_Based_Search_Geo
+cd fuzzy_attribute_based_search_geo
 echo '----do fuzzy_Attribute_Based_Search_Geo test--------------'
 python fuzzy_Attribute_Based_Search_Geo.py queriesAndResults.txt
 
