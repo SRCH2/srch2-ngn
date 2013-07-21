131a7baa5bef821a9a6c16161b91b44f641e5cbd
huaijie <huaijie@srch2.com>
2013-07-21 16:07:07 +0800

    polish renaming PREFIX/COMPLETE

--- test/wrapper/unit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hp/SRCH2/srch2-ngn/test/wrapper/unit
-# Build directory: /home/hp/SRCH2/srch2-ngn/test/wrapper/unit
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
