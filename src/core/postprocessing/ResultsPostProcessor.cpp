35b5e706bd97c25d3c5ad643f09fa848e1210abd
jamshid.esmaelnezhad <jamshid@srch2.com>
2013-07-22 11:57:46 -0700

    The parser skeleton is complete. Implementation will be started after the design is confirmed.

--- src/core/postprocessing/ResultsPostProcessor.cpp
+++ src/core/postprocessing/ResultsPostProcessor.cpp
@@ -18,7 +18,8 @@
  */
 
 
-#include "ResultsPostProcessor.h"
+#include "instantsearch/ResultsPostProcessor.h"
+#include "ResultsPostProcessorInternal.h"
 #include "RangeQueryFilter.h"
 
 #include "instantsearch/IndexSearcher.h"
@@ -28,54 +29,88 @@ namespace srch2
 namespace instantsearch
 {
 
-ResultsPostProcessor::ResultsPostProcessor(IndexSearcher *indexSearcher){
+
+//ResultsPostProcessor::ResultsPostProcessor(IndexSearcher *indexSearcher){
+//
+//
+//	IndexSearcherInternal * indexSearcherInternal = dynamic_cast<IndexSearcherInternal * >(indexSearcher);
+//	this->impl->forwardIndex = indexSearcherInternal->getForwardIndex();
+//	this->impl->schema = indexSearcherInternal->getSchema();
+//}
+//
+//ResultsPostProcessor::~ResultsPostProcessor(){
+//	// de-allocate plans
+//}
+//
+//
+//void ResultsPostProcessor::runPlan(Query * query, QueryResults * input, QueryResults *  output){
+//// run a plan by iterating on filters and running TODO
+//
+//	// short circuit in case the plan doesn't have any filters in it.
+//	ResultsPostProcessorPlan * plan = query->getPostProcessingPlan();
+//
+//
+//
+//
+//	// if no plan is set in Query, there is no post processing, just mirror the results
+//	if(plan == NULL){
+//		input->impl->copyForPostProcessing(output->impl);
+//		return;
+//	}
+//
+//	// iterating on filters and applying them on list of results
+//	QueryResults * inputOperand = input;
+//	plan->beginIteration();
+//	while(plan->hasMoreFilters()){
+//		ResultsPostProcessorFilter * filter = plan->nextFilter();
+//
+//		QueryResults outputOperand(input->impl->resultsFactory,input->impl->indexSearcherInternal,input->impl->query);
+//		filter->doFilter(this->impl->schema, this->impl->forwardIndex, query, inputOperand, &outputOperand);
+//
+//		if(!plan->hasMoreFilters()){ // TODO : after adding pointer logic reimplement this logic
+//			// copy new results to the output
+//			outputOperand.impl->copyForPostProcessing(output->impl);
+//		}else{
+//			// pass the input to the next filter
+//			inputOperand = &outputOperand;
+//		}
+//	}
+//	plan->closeIteration();
+//}
 
 
-	IndexSearcherInternal * indexSearcherInternal = dynamic_cast<IndexSearcherInternal * >(indexSearcher);
-	this->forwardIndex = indexSearcherInternal->getForwardIndex();
-	this->schema = indexSearcherInternal->getSchema();
+ResultsPostProcessorPlan::ResultsPostProcessorPlan(){
+	impl = new ResultsPostProcessorPlanInternal();
+	impl->iter = impl->plan.end();
+}
+ResultsPostProcessorPlan::~ResultsPostProcessorPlan(){
+	delete impl;
 }
 
-ResultsPostProcessor::~ResultsPostProcessor(){
-	// de-allocate plans
+void ResultsPostProcessorPlan::addFilterToPlan(ResultsPostProcessorFilter * filter){
+	impl->plan.push_back(filter);
 }
-
-
-void ResultsPostProcessor::runPlan(Query * query, QueryResults * input, QueryResults *  output){
-// run a plan by iterating on filters and running TODO
-
-	// short circuit in case the plan doesn't have any filters in it.
-	ResultsPostProcessorPlan * plan = query->getPostProcessingPlan();
-
-
-
-
-	// if no plan is set in Query, there is no post processing, just mirror the results
-	if(plan == NULL){
-		input->impl->copyForPostProcessing(output->impl);
-		return;
+void ResultsPostProcessorPlan::clearPlan(){
+	impl->plan.clear();
+}
+void ResultsPostProcessorPlan::beginIteration(){
+	impl->iter = plan.begin();
+}
+ResultsPostProcessorFilter * ResultsPostProcessorPlan::nextFilter(){
+	if(impl->iter == impl->plan.end()) return NULL;
+	ResultsPostProcessorFilter * result = *impl->iter;
+	++(impl->iter);
+	return result;
+}
+bool ResultsPostProcessorPlan::hasMoreFilters(){
+	if(impl->iter != impl->plan.end()) return true;
+	else{
+		return false;
 	}
-
-	// iterating on filters and applying them on list of results
-	QueryResults * inputOperand = input;
-	plan->beginIteration();
-	while(plan->hasMoreFilters()){
-		ResultsPostProcessorFilter * filter = plan->nextFilter();
-
-		QueryResults outputOperand(input->impl->resultsFactory,input->impl->indexSearcherInternal,input->impl->query);
-		filter->doFilter(this->schema, this->forwardIndex, query, inputOperand, &outputOperand);
-
-		if(!plan->hasMoreFilters()){ // TODO : after adding pointer logic reimplement this logic
-			// copy new results to the output
-			outputOperand.impl->copyForPostProcessing(output->impl);
-		}else{
-			// pass the input to the next filter
-			inputOperand = &outputOperand;
-		}
-	}
-	plan->closeIteration();
 }
-
+void ResultsPostProcessorPlan::closeIteration(){
+	impl->iter = impl->plan.end();
+}
 
 }
 }
