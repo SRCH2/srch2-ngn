c2e68a4c78088492afa62cc01b44ad6aed54fa8f
iman <iman@srch2.com>
2013-08-01 14:37:47 -0700

    adding tokenStream.h and tokenStreamContainer.h

---
+++ src/core/analyzer/TokenStreamContainer.h
@@ -0,0 +1,32 @@
+/*
+ * SharedToken.h
+ *
+ *  Created on: 2013-5-17
+ */
+
+#ifndef __CORE_ANALYZER__TOKENSTREAMCONTAINER_H__
+#define __CORE_ANALYZER__TOKENSTREAMCONTAINER_H__
+
+#include <vector>
+#include "util/encoding.h"
+#include "CharSet.h"
+
+namespace srch2
+{
+namespace instantsearch
+{
+
+class TokenStreamContainer {
+public:
+	/*
+	 * For example:  process "We went to school"
+	 * 		completeCharVector = "We went to school"
+	 * 		When we process to the first character 'W', currentToken="W", offset=0
+	 * 		When we move to the second character 'e', currentToken="We", offset=1
+	 */
+	std::vector<CharType> currentToken;			//current token
+	std::vector<CharType> completeCharVector; 	//complete char vector of a string
+	int offset;									//the offset of current position to process
+};
+}}
+#endif /* __CORE_ANALYZER__TOKENSTREAMCONTAINER_H__ */
