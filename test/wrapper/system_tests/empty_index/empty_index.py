6b75ad6b1d5fbd1386a0d869d5d22a6a8adb3c69
jiaying <jiaying@srch2.com>
2013-09-18 16:04:17 +0800

    insert a record and search it

--- test/wrapper/system_tests/empty_index/empty_index.py
+++ test/wrapper/system_tests/empty_index/empty_index.py
@@ -21,6 +21,17 @@ def testEmptyIndex(binary_path):
     os.popen(binary)
 
     pingServer()
+    #add an record
+    addCommand='curl "http://localhost:' + port + '/docs" -i -X PUT -d \'{"id":"1234", "name":"Toy Story", "category":"shop"}\''
+    os.system(addCommand)
+
+    time.sleep(11)
+    #do query
+    query='http://localhost:' + port + '/search?q=toy'
+    response = urllib2.urlopen(query).read()
+    response_json = json.loads(response)
+    if len(response_json['results']) != 1 or response_json['results'][0]['record']['id'] != "1234":
+       print 'test failed'
 
     #get pid of srch2-search-server and kill the process
     try:
