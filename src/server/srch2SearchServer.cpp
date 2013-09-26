2c5825df129dc8d89cc731dfec459614b5b120e4
Surendra Bisht <surendra@srch2.com>
2013-09-26 12:28:51 -0700

    reverting srch2serachserver changes not meant for this branch

--- src/server/srch2SearchServer.cpp
+++ src/server/srch2SearchServer.cpp
@@ -438,9 +438,6 @@ void parseProgramArguments(int argc, char** argv,
 
 pthread_t *threads;
 int MAX_THREADS;
-
-vector<struct event_base *> evbases;
-
 /**
  * Kill the server.  This function can be called from another thread to kill the server
  */
@@ -448,16 +445,9 @@ vector<struct event_base *> evbases;
 static void killServer(int signal) {
     Logger::console("Stopping server.");
     for (int i = 0; i < MAX_THREADS; i++) {
-            pthread_cancel(threads[i]);
-            Logger::console("Thread = <%u> stopped", threads[i]);
-        }
-
-//    for (int i = 0; i < evbases.size(); ++i) {
-//    	// call event_base_loopexit to terminate the thread's loop once it is done
-//    	// processing current event. This is a graceful shut down of each thread
-//    	event_base_loopbreak(evbases[i]);
-//    	event_base_dispatch(evbases[i]);
-//    }
+        pthread_cancel(threads[i]);
+        Logger::console("Thread = <%u> stopped", threads[i]);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -611,6 +601,7 @@ int main(int argc, char** argv) {
 
     int fd = bindSocket(http_addr, http_port);
     threads = new pthread_t[MAX_THREADS];
+    vector<struct event_base *> evbases;
     vector<struct evhttp *> http_servers;
     for (int i = 0; i < MAX_THREADS; i++) {
         evbase = event_init();
@@ -684,7 +675,6 @@ int main(int argc, char** argv) {
 
     for (int i = 0; i < MAX_THREADS; i++) {
         pthread_join(threads[i], NULL);
-        Logger::console("Thread = <%u> stopped", threads[i]);
     }
 
     delete[] threads;
