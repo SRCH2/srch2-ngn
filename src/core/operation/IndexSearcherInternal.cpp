eff5915d34cfe61c3a046e0dfde3c9301489e7c9
jamshid.esmaelnezhad <jamshid@srch2.com>
2013-10-17 23:09:14 -0700

    A bug related to active node caching is fixed.

--- src/core/operation/IndexSearcherInternal.cpp
+++ src/core/operation/IndexSearcherInternal.cpp
@@ -558,7 +558,11 @@ int IndexSearcherInternal::searchTopKQuery(const Query *query, const int offset,
         	if(query->getQueryTerms()->size() == 1){ // for example : q=a
         		unsigned numberOfResults = searchTopKFindResultsForOnlyOnePopularKeyword(query, activeNodesVector.at(0) ,
         				offset + nextK - queryResults->getNumberOfResults() , queryResults);
-        		delete activeNodesVector.at(0);
+        	    if (activeNodesVector.at(0)->isResultsCached() == true){
+        	    	activeNodesVector.at(0)->busyBit->setFree();
+        	    }else{
+            		delete activeNodesVector.at(0);
+        	    }
                 queryResultsInternal->finalizeResults(this->indexData->forwardIndex);
         		return numberOfResults;
         	}
