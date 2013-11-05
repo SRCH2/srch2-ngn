961cdb21d2d9dc2d0e44939da84904b7f492831b
atwalr <Iman>
2013-11-05 11:11:02 -0800

    add author info

--- src/java/jni/SearchableString.cpp
+++ src/java/jni/SearchableString.cpp
@@ -1,3 +1,11 @@
+
+/*****************************************************************************
+ *                                                                           *
+ *              AUTHOR : RJ ATWAL                                            *
+ *                                                                           *
+ *                                                                           * 
+  ****************************************************************************/
+
 #include "SearchableString.h"
 #include "util/utf8/unchecked.h"
 #include "util/utf16/utf16.h"
@@ -46,3 +54,4 @@ jobject JNIClass::SearchableString::createNew(std::string& content) const {
      in the JVM, and returns a handle to the new object */
   return env->NewObject(this->classPtr, constructor, internalString);
 }
+
