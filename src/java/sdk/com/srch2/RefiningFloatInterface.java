4032da87f4bdee17bd06c4980ced17d38f0cefe4
atwalr <Iman>
2013-11-08 18:37:20 -0800

    added Float Attribute Tests

---
+++ src/java/sdk/com/srch2/RefiningFloatInterface.java
@@ -0,0 +1,15 @@
+
+/*****************************************************************************
+ *                                                                           *
+ *              AUTHOR : RJ ATWAL                                            *
+ *                                                                           *
+ *                                                                           * 
+  ****************************************************************************/
+
+package com.srch2;
+
+
+public interface RefiningFloatInterface extends Attribute<Float>, Refining {
+  public float getFloat();
+}
+
