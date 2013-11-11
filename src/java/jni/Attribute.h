2564a0a32c41bd89e1ed0272ef3bda7165c4ee83
atwalr <Iman>
2013-11-11 07:55:47 -0800

    added comments to Attribute structure

--- src/java/jni/Attribute.h
+++ src/java/jni/Attribute.h
@@ -37,13 +37,18 @@ namespace JNIClass {
     /** test if jobject is an instance of the java StringAttribute class */
      jboolean isInstance(jobject obj) const;
 
+    /** creates a new Attribute value with the equivalent value as the given
+        c++ instance with the function C::makeJValue applied to it */
+    template <typename C> jobject createNew(T& content) const;
     /** creates a new Attribute java instance with the equivalant value
         of the given c++ object */
-    template <typename C> jobject createNew(T& content) const;
     jobject createNew(T& content) const;
+
     /** Converts the given Attribute java instance to an instance of the 
-        given c++ type using the provided Function */
+        given c++ type using the provided Function: C::convertJavaToCPP */
     template <typename C> T toValue(jobject&) const;
+    /** Directly constructs an instance of the given c++ type from the given
+        Attribute java instance. */
     T toValue(jobject&) const;
 
     ~Attribute() {
@@ -51,45 +56,45 @@ namespace JNIClass {
     }
   };
 
-template <typename T>
-jboolean Attribute<T>::isInstance(jobject obj) const {
-  return env->IsAssignableFrom(env->GetObjectClass(obj), classPtr);
-}
-
-/*  ---- toValue Specializations --- */
-
-template <typename T> template <typename C> inline  
-T JNIClass::Attribute<T>::toValue(jobject& obj) const {
-  return T(C::convertJavaToCPP(*env, env->CallObjectMethod(obj, getValue)));
-}
-
-template <typename T>
-T JNIClass::Attribute<T>::toValue(jobject& obj) const {
-  return T(env->CallObjectMethod(obj, getValue));
-}
-
-template <> inline  
-int Attribute<int>::toValue(jobject& obj) const {
-  return env->CallIntMethod(obj, getValue);
-}
-
-template <> inline 
-float Attribute<float>::toValue(jobject& obj) const { 
-  return env->CallFloatMethod(obj, getValue);
-}
-
-
-/* ---- createNew Specializations --- */
-
-template <typename T> template<typename C> inline
-jobject JNIClass::Attribute<T>::createNew(T& content) const {
-  return env->NewObject(classPtr, constructor, C::makeJValue(*env, content));
-}
-
-template <typename T> 
-jobject Attribute<T>::createNew(T& content) const {
-  return env->NewObject(classPtr, constructor, content);
-}
+  template <typename T>
+  jboolean Attribute<T>::isInstance(jobject obj) const {
+    return env->IsAssignableFrom(env->GetObjectClass(obj), classPtr);
+  }
+  
+  /*  ---- toValue Specializations --- */
+  
+  template <typename T> template <typename C> inline  
+  T JNIClass::Attribute<T>::toValue(jobject& obj) const {
+    return T(C::convertJavaToCPP(*env, env->CallObjectMethod(obj, getValue)));
+  }
+  
+  template <typename T>
+  T JNIClass::Attribute<T>::toValue(jobject& obj) const {
+    return T(env->CallObjectMethod(obj, getValue));
+  }
+  
+  template <> inline  
+  int Attribute<int>::toValue(jobject& obj) const {
+    return env->CallIntMethod(obj, getValue);
+  }
+  
+  template <> inline 
+  float Attribute<float>::toValue(jobject& obj) const { 
+    return env->CallFloatMethod(obj, getValue);
+  }
+  
+  
+  /* ---- createNew Specializations --- */
+  
+  template <typename T> template<typename C> inline
+  jobject JNIClass::Attribute<T>::createNew(T& content) const {
+    return env->NewObject(classPtr, constructor, C::makeJValue(*env, content));
+  }
+  
+  template <typename T> 
+  jobject Attribute<T>::createNew(T& content) const {
+    return env->NewObject(classPtr, constructor, content);
+  }
 
 
 }
--- src/java/jni/AttributeString.h
+++ src/java/jni/AttributeString.h
@@ -1,7 +1,7 @@
 #ifndef __ATTRIBUTE_STRING_H__
 #define __ATTRIBUTE_STRING_H__
 
-/*****************************************************************************
+/*********************6********************************************************
  *                                                                           *
  *              AUTHOR : RJ ATWAL                                            *
  *                                                                           *
@@ -12,65 +12,76 @@
 #include "util/utf8/unchecked.h"
 #include "util/utf16/utf16.h"
 
+/* Case encasing logic for converting a UTF16 java string into an equivalent
+   UTF8 encoded std::string */
 namespace JNIClass {
   struct MakeUTF8StringFromUTF16JString {
-  static void inline creatNew(std::string& content, jchar* buffer,
-      JNIEnv &env, jstring& internalString) {
-    jchar *end;
-
-    /* convert a UTF8 string to a UTF16 encoded array*/
-    end= utf8::unchecked::utf8to16(content.begin(), content.end(), buffer);
-
-    /* Creates a new String on the Java heap */
-    internalString= (jstring) env.NewString(buffer, end - buffer);
-  }
-
-  static std::string convertJavaToCPP(JNIEnv& env, jobject srch2String) {
-    const jchar *utf16CharStringValue=
-      env.GetStringChars((jstring)srch2String, JNI_FALSE);
+    /* creates a new java string with UTF16 encoding passed as an out parameter 
+       from the given std::string; a temporary buffer is also given to perform
+       the necessary conversion */
+    private:
+    static void inline creatNew(std::string& content, jchar* buffer,
+        JNIEnv &env, jstring& internalString) {
+      jchar *end;
   
-    /* converts a UTF16 char array to UTF8 */
-    utf8::uint16_t* utf16Start= (utf8::uint16_t*) utf16CharStringValue;
-    utf8::uint16_t* utf16CharString= (utf8::uint16_t*) utf16CharStringValue;
-    utf8::uint16_t* utf16End= (utf8::uint16_t*) 
+      /* convert a UTF8 string to a UTF16 encoded array*/
+      end= utf8::unchecked::utf8to16(content.begin(), content.end(), buffer);
+  
+      /* Creates a new String on the Java heap */
+      internalString= (jstring) env.NewString(buffer, end - buffer);
+    }
+    public:
+    /* return a std::string encoding in UTF8 with value equivalent to the
+       given java UTF16 string */
+    static std::string convertJavaToCPP(JNIEnv& env, jobject srch2String) {
+      const jchar *utf16CharStringValue=
+        env.GetStringChars((jstring)srch2String, JNI_FALSE);
+    
+      /* converts a UTF16 char array to UTF8 */
+      utf8::uint16_t* utf16Start= (utf8::uint16_t*) utf16CharStringValue;
+      utf8::uint16_t* utf16CharString= (utf8::uint16_t*) utf16CharStringValue;
+      utf8::uint16_t* utf16End= (utf8::uint16_t*) 
       ((char*) utf16Start +
       utf16::byteLength(env.GetStringLength((jstring) srch2String),
         utf16CharString));
-    char utf8Start[1024];
-    char *utf8End= utf8::unchecked::utf16to8(utf16Start, utf16End, utf8Start);
+      char utf8Start[1024];
+      char *utf8End=
+        utf8::unchecked::utf16to8(utf16Start, utf16End, utf8Start);
   
-    return std::string(utf8Start, utf8End);
-  }
-  
-  static jvalue makeJValue(JNIEnv& env, std::string& content) {
-  /* Checks the size of input string to see if it can be 
-     handled on the stack*/
-  jchar *buffer;
-  jvalue internalString;
-  jobject rtn;
-
-  if((content.length() < 512)) {
-    jchar uft16start[1024];
-    buffer= uft16start;
-  } else {
-    buffer= new jchar[content.length() * 2];
-  }
-  /* Creates a new Object of StringAttribute type on the java heap using
-     the constructor specified with internalString as an argument.
-
-     Runs 
-         new StringAttribute(internalString)
-
-     in the JVM, and returns a handle to the new object */
-  MakeUTF8StringFromUTF16JString::creatNew(content,
-      buffer, env, (jstring&) internalString.l);
+        return std::string(utf8Start, utf8End);
+      }
     
-  /* free heap memory if needed */
-  if((content.length() >= 512)) 
-    delete buffer;
+    /* returns a reference to a newly created UFT16 encoded java string with
+       value equivalent to the given UTF8 encoded std::string */
+    static jvalue makeJValue(JNIEnv& env, std::string& content) {
+    /* Checks the size of input string to see if it can be 
+       handled on the stack*/
+    jchar *buffer;
+    jvalue internalString;
+    jobject rtn;
   
-  return internalString;
-}
+    if((content.length() < 512)) {
+      jchar uft16start[1024];
+      buffer= uft16start;
+    } else {
+      buffer= new jchar[content.length() * 2];
+    }
+    /* Creates a new Object of StringAttribute type on the java heap using
+       the constructor specified with internalString as an argument.
+  
+       Runs 
+           new StringAttribute(internalString)
+  
+       in the JVM, and returns a handle to the new object */
+    MakeUTF8StringFromUTF16JString::creatNew(content,
+        buffer, env, (jstring&) internalString.l);
+      
+    /* free heap memory if needed */
+    if((content.length() >= 512)) 
+      delete buffer;
+  
+    return internalString;
+  }
 };
 }
 #endif /* __STRING_ATTRIBUTE_H__ */
