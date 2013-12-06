538a209a6f1ba1c8f6020f543e80534ec2cd44a0
rj <rj@srch2.com>
2013-12-06 00:53:37 -0800

    added a test case for qf feature

--- test/wrapper/system_tests/main.sh
+++ test/wrapper/system_tests/main.sh
@@ -9,6 +9,16 @@ cd $SYSTEM_TEST_DIR
 # We remove the old indexes, if any, before doing the test.
 rm -rf data/ 
 
+test_id="qf_dynamic_ranking"
+echo "---------------------do $test_id-----------------------"
+python ./qf_dynamic_ranking/qf_dynamic_ranking.py $SRCH2_ENGINE_DIR ./qf_dynamic_ranking/queriesAndResults.txt > system_test.log 2>&1
+
+if [ $? -gt 0 ]; then
+    echo "FAILED: $test_id"
+    exit -1
+fi
+echo "-- PASSED: $test_id"
+
 test_id="phrase search test"
 echo "---------------------do $test_id-----------------------"
 python ./phraseSearch/phrase_search.py $SRCH2_ENGINE_DIR ./phraseSearch/queries.txt > system_test.log 2>&1
@@ -264,6 +274,8 @@ if [ $? -gt 0 ]; then
     exit -1
 fi
 echo "-- PASSED: $test_id"
+
+
 
 
 # clear the output directory. First make sure that we are in correct directory
