c80a8198ae4753c9abc2c89c30627fe9e953c0fa
jamshid.esmaelnezhad <jamshid@srch2.com>
2013-12-29 00:00:35 -0800

    Q.O. set to choose the 2nd plan always.

--- src/core/operation/QueryOptimizer.cpp
+++ src/core/operation/QueryOptimizer.cpp
@@ -346,7 +346,7 @@ void QueryOptimizer::injectRequiredSortOperators(PhysicalPlanOptimizationNode * 
 PhysicalPlanOptimizationNode * QueryOptimizer::findTheMinimumCostTree(vector<PhysicalPlanOptimizationNode *> & treeOptions, PhysicalPlan & physicalPlan){
 	PhysicalPlanOptimizationNode * minPlan = NULL;
 	unsigned minCost = 0;
-	minPlan = treeOptions.at(0);
+	minPlan = treeOptions.at(1);
 //	for(vector<PhysicalPlanOptimizationNode *>::iterator treeOption = treeOptions.begin() ; treeOption != treeOptions.end() ; ++treeOption){
 //		PhysicalPlanCost cost;
 //		unsigned numberOfGetNextCalls = 0;
