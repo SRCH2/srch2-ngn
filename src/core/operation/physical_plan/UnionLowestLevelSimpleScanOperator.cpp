8976054e0d41c5011373fc76bb57ee412bf68bb7
jamshid.esmaelnezhad <jamshid@srch2.com>
2014-02-12 18:53:58 -0800

    A mistake in costing is fixed.

--- src/core/operation/physical_plan/UnionLowestLevelSimpleScanOperator.cpp
+++ src/core/operation/physical_plan/UnionLowestLevelSimpleScanOperator.cpp
@@ -246,6 +246,7 @@ PhysicalPlanCost UnionLowestLevelSimpleScanOptimizationOperator::getCostOfGetNex
 
 	PhysicalPlanCost resultCost;
 	resultCost.cost = 1;
+	resultCost.cost = resultCost.cost * 0.1;
 	return resultCost; // cost of sequential access
 }
 // the cost of close of a child is only considered once since each node's close function is only called once.
