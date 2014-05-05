a77c74020bb2c62a5047936c36067b67dc152155
Prateek Verma <prateek@srch2.com>
2014-05-05 11:53:07 -0700

    corrected a filename in code and added readme file

--- test/sharding/integration/exact_a1/testInsertAndQuery.py
+++ test/sharding/integration/exact_a1/testInsertAndQuery.py
@@ -1,6 +1,6 @@
 #This tests insertion and querying
 #To run the code go to the parent directory and execute following command
-# python ./exact_a1/test_insertAndQuery.py ../../../build/src/server/srch2-search-server ./exact_a1/insertFile.txt ./exact_a1/nodesList.txt
+# python ./exact_a1/testInsertAndQuery.py ../../../build/src/server/srch2-search-server ./exact_a1/insertFile.txt ./exact_a1/nodesList.txt
 # python <python code> <srch2-engine binary> <path to the file that contains records for insertion/querying> <path to the file containing list of nodes>
 
 
