687ae6a39743f575d4f99556dab103dadec82c1d
Prateek Verma <prateek@srch2.com>
2014-05-06 17:30:36 -0700

    added two test cases

--- test/sharding/integration/2n-insertA-queryB/testInsertAndQuery.py
+++ test/sharding/integration/2n-insertA-queryB/testInsertAndQuery.py
@@ -1,6 +1,6 @@
 #This tests insertion and querying
 #To run the code go to the parent directory and execute following command
-# python ./exact_a1/testInsertAndQuery.py ../../../build/src/server/srch2-search-server ./exact_a1/insertFile.txt ./exact_a1/nodesList.txt
+#python ./2n-insertA-queryB/testInsertAndQuery.py ../../../build/src/server/srch2-search-server ./2n-insertA-queryB/2n-insertA-queryB.txt ./2n-insertA-queryB/listOfNodes.txt 
 # python <python code> <srch2-engine binary> <path to the file that contains records for insertion/querying> <path to the file containing list of nodes>
 
 
--- test/sharding/integration/2n-insertAB-queryAB/testInsertAndQuery.py
+++ test/sharding/integration/2n-insertAB-queryAB/testInsertAndQuery.py
@@ -1,6 +1,6 @@
 #This tests insertion and querying
 #To run the code go to the parent directory and execute following command
-# python ./exact_a1/testInsertAndQuery.py ../../../build/src/server/srch2-search-server ./exact_a1/insertFile.txt ./exact_a1/nodesList.txt
+# python ./2n-insertAB-queryAB/testInsertAndQuery.py ../../../build/src/server/srch2-search-server ./2n-insertAB-queryAB/2n-insertAB-queryAB.txt ./2n-insertAB-queryAB/listOfNodes.txt
 # python <python code> <srch2-engine binary> <path to the file that contains records for insertion/querying> <path to the file containing list of nodes>
 
 
