7eabfd71ef0a2ce1f0f21829da25c63a24cc47a2
Surendra Bisht <surendra@srch2.com>
2014-05-27 21:28:48 -0700

    moving SM initialzation before RM intialization to avoid unnecessary timeouts

--- src/server/srch2SearchServer.cpp
+++ src/server/srch2SearchServer.cpp
@@ -814,6 +814,13 @@ int main(int argc, char** argv) {
 
 	// create Transport Module
 	transportManager = new srch2http::TransportManager(evBases, nodes);
+	// run SM
+	unsigned masterNodeId =  serverConf->getCluster()->getNodes()->at(0).getId(); // temporary for V0
+	srch2http::Synchronizer  *syncManager = new srch2http::Synchronizer(*serverConf ,
+			*transportManager, masterNodeId);
+	pthread_t *synchronizerThread = new pthread_t;
+	pthread_create(synchronizerThread, NULL, srch2http::bootSynchronizer, (void *)syncManager);
+
 	// create Routing Module
 	srch2http::RoutingManager *routesManager =
 			new srch2http::RoutingManager(*serverConf, *transportManager);
@@ -823,13 +830,6 @@ int main(int argc, char** argv) {
 	// create DP external
 	srch2http::DPExternalRequestHandler *dpExternal =
 			new srch2http::DPExternalRequestHandler(serverConf, routesManager);
-
-	// run SM
-	unsigned masterNodeId =  serverConf->getCluster()->getNodes()->at(0).getId(); // temporary for V0
-	srch2http::Synchronizer  *syncManager = new srch2http::Synchronizer(*serverConf ,
-			*transportManager, masterNodeId);
-	pthread_t *synchronizerThread = new pthread_t;
-	pthread_create(synchronizerThread, NULL, srch2http::bootSynchronizer, (void *)syncManager);
 
 	// create DPExternal,core pairs
 	// map from coreId to DPExternalCoreHandle which is a container of
