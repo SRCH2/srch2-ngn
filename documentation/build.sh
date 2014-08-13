ecf8d30f418bbc1e128b404ed537135d7dca5be6
Jianfeng <jianfengjia@srch2.com>
2014-08-12 17:26:51 -0700

    fix the bulid.sh to not overwrite the old version

--- documentation/build.sh
+++ documentation/build.sh
@@ -17,27 +17,50 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-# create site folder 
-mkdocs build
-[ $? -ne 0 ] && { echo $'\n Mkdocs build to "site" folder failed, Stop the release'; exit -1;}
+red='\e[0;31m'
+NC='\e[0m' # No Color
 
 sdk_source="../source/SRCH2-Android-SDK/"
 target="$sdk_source/target"
 deploy="${target}/deploy"
+
+echo "Starting the release ..."
+
+# reminder to change the pom.xml
+cd $sdk_source
+version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+cd $OLDPWD
+
+[[ $version == *SNAPSHOT ]] && { echo "ERROR:We should never release the SNAPSHOT version, please change the pom.xml" ; exit -1;}
+
+echo "Don't forget to change the project.version in the pom.xml!"
+echo -e "Detected releaes version is ${red} $version ${NC}, is this correct? [y|N]"
+read yes
+[ "$yes" == "y" ] || { echo "Stopped."; exit -1; }
+
+release_file="./release_notes/release_$version"
+[ -f "$release_file" ] || { echo "ERROR:$release_file doesn't exist! Stopped."; exit -1; }
+
+\rm -rf site
+mkdocs build
+[ $? -ne 0 ] && { echo $'\nERROR: Mkdocs build to "site" folder failed, Stop the release'; exit -1;}
+
 cd $sdk_source
 mvn clean deploy
-[ $? -ne 0 ] && { echo $'\n Error happens while deploying, Stop the release'; exit -1;}
+[ $? -ne 0 ] && { echo $'\nERROR: Error happens while deploying, Stop the release'; exit -1;}
 
 # This javadoc:jar command will produce the cleanner java doc site.
 mvn javadoc:jar
-[ $? -ne 0 ] && { echo $'\n Error happens while creating javadoc, Stop the release'; exit -1;}
+[ $? -ne 0 ] && { echo $'\nERROR: Error happens while creating javadoc, Stop the release'; exit -1;}
 cd $OLDPWD
 
-cp ${deploy}/com/srch2/*/*/*.{jar,aar} ./site/download/latest-releases/
+cp ${deploy}/com/srch2/*/*/*.{jar,aar} ./site/download/releases/
+cp ${release_file} ./site/download/releases/
 cp -r ${target}/apidocs ./site/javadoc
 
 echo $'\nLocal release is done, wait for sync to the webmaster'
 
-scp -r ${deploy}/* rvijax@web240.webfaction.com:~/webapps/srch2/repo/maven/
-scp -r ./site/* rvijax@web240.webfaction.com:~/webapps/srch2/android/
+rsync -rav --ignore-existing ${deploy}/* rvijax@web240.webfaction.com:~/webapps/srch2/repo/maven/
+rsync -rav ./site/ rvijax@web240.webfaction.com:~/webapps/srch2/android/releases/$version/
 
+echo "Done. Version is $version"
--- documentation/docs/download.md
+++ documentation/docs/download.md
@@ -1,8 +1,8 @@
 ﻿
 ###SRCH2 Android SDK
 
-You can download the latest release of the SRCH2 Android SDK by clicking [here](../download/latest-releases/SRCH2-Android-SDK.aar)
+You can download the latest release of the SRCH2 Android SDK by clicking [here](../download/releases/)
 
 ###Hello SRCH2 Search Demo
 
-You can download the Android application project for the Hello SRCH2 Search Demo tutorial by clicking [here](../download/latest-releases/hello-srch2-search-demo-project.zip)
+You can clone the Android application project for the Hello SRCH2 Search Demo tutorial from [github](https://github.com/SRCH2/hello-srch2-android-sdk)
