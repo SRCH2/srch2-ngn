1cc370e33d3e0d414aae2e14cb43b4b5feed9d60
liusrch2 <liu@srch2.com>
2014-08-14 14:04:53 -0700

    Formatted

--- include/instantsearch/Constants.h
+++ include/instantsearch/Constants.h
@@ -145,13 +145,13 @@ typedef enum
 // change the names, they are too general
 typedef enum
 {
-    ATTRIBUTE_TYPE_FLOAT ,
-    ATTRIBUTE_TYPE_TEXT ,
-    ATTRIBUTE_TYPE_TIME ,// Time is kept as a long integer in the core.
+    ATTRIBUTE_TYPE_FLOAT,
+    ATTRIBUTE_TYPE_TEXT,
+    ATTRIBUTE_TYPE_TIME,// Time is kept as a long integer in the core.
     ATTRIBUTE_TYPE_LONG,
     ATTRIBUTE_TYPE_INT,
     ATTRIBUTE_TYPE_DOUBLE,
-         // The meaning of this long integer is the number of seconds past from January 1st, 1970
+    // The meaning of this long integer is the number of seconds past from January 1st, 1970
     // TypedValue class uses these constants to understand if it is dealing with a single-valued attribute
     // or a multi-valued one.
     ATTRIBUTE_TYPE_MULTI_INT,
