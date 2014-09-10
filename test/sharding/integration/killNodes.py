0fb0cd34ac4cadea1ff1a84cbc3980378ccfea60
Prateek Verma <prateek@calvin.calit2.uci.edu>
2014-09-09 17:43:07 -0700

    increased merge time

--- test/sharding/integration/killNodes.py
+++ test/sharding/integration/killNodes.py
@@ -1,6 +1,13 @@
-import commands
+#To kill the engine
+#Release mode
+#python killNodes.py release
+#Debug mode
+#python killNodes.py debug
+
+import commands,sys
+
 if __name__ == '__main__':
-    pid = commands.getstatusoutput('pgrep -f ../../../build/src/server/srch2-search-server')
+    pid = commands.getstatusoutput('pgrep -f ../../../build/src/server/srch2-search-server-'+sys.argv[1])
     processId = pid[1].split()
     for i in range(len(processId)-1):
         killNodesOutput = commands.getstatusoutput('kill -9 ' + processId[i])
