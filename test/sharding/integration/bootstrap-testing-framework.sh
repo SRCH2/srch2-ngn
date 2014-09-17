887cf16e13748b9192f72c1a786082804bdc9a64
jamshid@srch2.com <jamshid@calvin.calit2.uci.edu>
2014-09-17 14:23:10 -0700

    Bootstrap script completed.

--- test/sharding/integration/bootstrap-testing-framework.sh
+++ test/sharding/integration/bootstrap-testing-framework.sh
@@ -1,1 +1,8 @@
 ln -s ~chrysler/comparison-data/stackoverflow/server/data/sharding-test/node-intensive-novalidation/* ./nodeIntensive-noValidation/
+ln -s ~chrysler/comparison-data/stackoverflow/server/data/sharding-test/node-intensive/* ./nodeIntensive/
+ln -s ~chrysler/comparison-data/stackoverflow/server/data/sharding-test/data-intensive/* ./dataIntensive/
+
+ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/node-intensive-novalidation/* nodeIntensive-noValidation/
+ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/node-intensive/* nodeIntensive/
+ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/data-intensive/* ./dataIntensive/
+
