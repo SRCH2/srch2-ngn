542fb5ba9dab9613da0b8d7634c235055b645119
Prateek Verma <prateek@calvin.calit2.uci.edu>
2014-09-25 14:39:33 -0700

    updated bootstrap.sh and modified node address file

--- test/sharding/integration/bootstrap-testing-framework.sh
+++ test/sharding/integration/bootstrap-testing-framework.sh
@@ -6,3 +6,7 @@ ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/node-
 ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/node-intensive/* nodeIntensive/
 ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/data-intensive/* ./dataIntensive/
 mkdir -p dashboardFiles/nodeIntensive-noValidation/
+
+ln -s ~chrysler/comparison-data/stackoverflow/server/data/sharding-test/node-intensive-novalidation/* ./test_dynamicShards_noValidation/
+ln -s ~chrysler/comparison-data/stackoverflow/server/queries/sharding-test/node-intensive-novalidation/* ./test_dynamicShards_noValidation/
+
