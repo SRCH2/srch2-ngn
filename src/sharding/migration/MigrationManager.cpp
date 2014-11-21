c8ffb5cf606e30ce87dbe4857c1642c2530ee901
jamshid.esmaelnezhad <jamshid@srch2.com>
2014-11-21 09:25:42 -0800

    Fix try.

--- src/sharding/migration/MigrationManager.cpp
+++ src/sharding/migration/MigrationManager.cpp
@@ -488,7 +488,7 @@ void MigrationService::receiveShard(ClusterShardId shardId, unsigned remoteNode)
 		migrationMgr->sendComponentDoneMsg(currentSessionInfo);
 	}
 
-//	migrationMgr->sendComponentDoneMsg(currentSessionInfo);
+	migrationMgr->sendComponentDoneMsg(currentSessionInfo);
 
 	// create an empty shard
 	if (emptyshard) {
