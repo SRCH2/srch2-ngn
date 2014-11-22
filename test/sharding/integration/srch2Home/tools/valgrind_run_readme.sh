de4858f6e19db2fca9dfa7c712ea62a18238b50c
jamshid.esmaelnezhad <jamshid@srch2.com>
2014-11-21 23:20:04 -0800

    valgrind command enriched.

--- test/sharding/integration/srch2Home/tools/valgrind_run_readme.sh
+++ test/sharding/integration/srch2Home/tools/valgrind_run_readme.sh
@@ -1,2 +1,3 @@
-valgrind --leak-check=full --suppressions=./suppressions_srch2.supp --error-limit=no --track-origins=yes ../../../../../build/src/server/srch2-search-server --config=$1
+#valgrind --leak-check=full --suppressions=./suppressions_srch2.supp --error-limit=no --track-origins=yes ../../../../../build/src/server/srch2-search-server --config=$1
+valgrind --tool=memcheck --leak-check=full --num-callers=5 --show-reachable=yes --track-fds=yes --suppressions=./suppressions_srch2.supp --error-limit=no ../../../../../build/src/server/srch2-search-server --config=$1
 #valgrind --tool=massif --error-limit=no ../../../../../build/src/server/srch2-search-server --config=$1
