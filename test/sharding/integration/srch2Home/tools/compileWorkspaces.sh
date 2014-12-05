fb67037f1511216ae1c53af15597c22241109eb3
jamshid.esmaelnezhad <jamshid@srch2.com>
2014-12-05 13:31:28 -0800

    Transactions that lock shard ids must not hold on to the readview

--- test/sharding/integration/srch2Home/tools/compileWorkspaces.sh
+++ test/sharding/integration/srch2Home/tools/compileWorkspaces.sh
@@ -1,14 +1,14 @@
 currentDir=`pwd`
 cd ~/workspace-srch2-v4/srch2-ngn/build;
 cmake ..;
-make -j 8;
+make -j 8 srch2-search-server;
 cd ~/workspace-srch2-1/srch2-ngn/build;
 cmake ..;
-make -j 8;
+make -j 8 srch2-search-server;
 cd ~/workspace-srch2-2/srch2-ngn/build;
 cmake ..;
-make -j 8;
+make -j 8 srch2-search-server;
 cd ~/workspace-srch2-3/srch2-ngn/build;
 cmake ..;
-make -j 8;
+make -j 8 srch2-search-server;
 cd $currentDir
