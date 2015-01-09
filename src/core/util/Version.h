fb4aba080529b6b84d7e62b59f32187112e20341
Surendra Bisht <surendra@srch2.com>
2015-01-08 22:36:42 -0800

    bumping version

--- src/core/util/Version.h
+++ src/core/util/Version.h
@@ -1,8 +1,8 @@
 #ifndef __CORE_UTIL_VERSION_H__
 #define __CORE_UTIL_VERSION_H__
 
-#define ENGINE_VERSION "4.4.2"
-#define INDEX_VERSION 6 // Increment this every time we make some changes to indexes
+#define ENGINE_VERSION "4.4.3"
+#define INDEX_VERSION 7 // Increment this every time we make some changes to indexes
 #include <string>
 /**
  *  Helper class for version system. 
