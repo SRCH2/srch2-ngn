cd957aabd489a10db3ca47f95fd25a5571f9f176
Jamshid Esmaelnezhad <jamshid@srch2.com>
2015-01-19 05:01:43 -0800

    Bug fix

--- test/sharding/integration/mini_cluster/init-cluster.sh
+++ test/sharding/integration/mini_cluster/init-cluster.sh
@@ -58,8 +58,8 @@ do
    if [[ $formatAll -eq 1 ]] ; then
       ssh ${__LOGIN_USERS[$i]}@${__IP_ADDRESSES[$i]} rm -r ${__SRCH2_HOMES[$i]}
    fi
-   ssh ${__LOGIN_USERS[$i]}@${__IP_ADDRESSES[$i]} mkdir ${__SRCH2_HOMES[$i]}
-   ssh ${__LOGIN_USERS[$i]}@${__IP_ADDRESSES[$i]} mkdir ${__SRCH2_HOMES[$i]}/$__BIN_DIR_NAME
+   ssh ${__LOGIN_USERS[$i]}@${__IP_ADDRESSES[$i]} mkdir -p ${__SRCH2_HOMES[$i]}
+   ssh ${__LOGIN_USERS[$i]}@${__IP_ADDRESSES[$i]} mkdir -p ${__SRCH2_HOMES[$i]}/$__BIN_DIR_NAME
    for f in "${__INSTALL_FILES_SCRIPTS[@]}"
    do
       ###### Only upload the modified scripts  ######
