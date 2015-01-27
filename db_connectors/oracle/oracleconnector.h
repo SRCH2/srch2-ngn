000f0c904d038bf1c6db5282f792cbaf2481bb98
liusrch2 <liu@srch2.com>
2015-01-26 22:58:28 -0800

    Added Oracle db connector and SDK doc (doc needs to be improved)

---
+++ db_connectors/oracle/oracleconnector.h
@@ -0,0 +1,120 @@
+/*
+ * oracleconnector.h
+ *
+ *  Created on: Jan 21, 2015
+ *      Author: Chen Liu liu@srch2.com
+ */
+
+#ifndef __ORACLECONNECTOR_H__
+#define __ORACLECONNECTOR_H__
+
+#include "DataConnector.h"
+#include <string>
+#include <sqlext.h>
+#include <sql.h>
+#include <vector>
+
+class OracleConnector: public DataConnector {
+public:
+    OracleConnector();
+    virtual ~OracleConnector();
+
+    /*
+     * Initialize the connector. Establish a connection to the Oracle Database.
+     * The init() function should implement the following things :
+     *
+     * 1. Pass the ServerInterface handle from the engine to the connector so
+     * that the connector can use function "insertRecord", "deleteRecord",
+     * "updateRecord" and "configLookUp".
+     *
+     * 2. Check if the config file contains all the required parameters.
+     *
+     * 3. Connect to the database.
+     *
+     * 4. Get the schema information from the database.
+     */
+    virtual int init(ServerInterface *serverHandle);
+
+    /*
+     * Retrieve records from the table records and insert them into the SRCH2 engine.
+     * Query : SELECT * FROM [TABLE];
+     */
+    virtual int createNewIndexes();
+
+    /*
+     * Periodically Pull the updates from the Oracle change table, and send
+     * corresponding requests to the SRCH2 engine.
+     * For example: table emp(id, name, age, salary).
+     * Change table name : emp_ct
+     * Query : SELECT RSID$, OPERATION$, id, name, age, salary
+     *         FROM emp_ct
+     *         WHERE RSID$ > ?;
+     * Query is using prepared statement where "?" is the last accessed record RSID$.
+     * The connector always keeps the latest RSID$ so that the connector can skip the
+     * processed record.
+     */
+    virtual int runListener();
+
+    //Save the lastAccessedLogRecordRSID to the file
+    //For Oracle, we save the RSID instead of time stamp.
+    virtual void saveLastAccessedLogRecordTime();
+private:
+    //Config parameters
+    SQLHENV henv;
+    SQLHDBC hdbc;
+
+    int listenerWaitTime;
+    int oracleMaxColumnLength;
+    ServerInterface *serverHandle;
+
+    //Storing the table schema information
+    std::vector<std::string> fieldNames;
+
+    /*
+     * The last time SQL Server Connector accessed the Change table version,
+     * We are using the version instead of time stamp in SQL Server but
+     * the idea is same.
+     */
+    long int lastAccessedLogRecordRSID;
+
+    //Execute the query.
+    bool executeQuery(SQLHSTMT & hstmt, const std::string & query);
+
+    void printSQLError(SQLHSTMT & hstmt);   //Log the SQL Server error msg.
+
+    /*
+     * Connect to the Oracle database by using unixODBC.
+     * Data Source is the name in /etc/odbcinst.ini
+     */
+    bool connectToDB();
+
+    /*
+     * Check if the config file has all the required parameters.
+     * e.g. if it contains dataSource, table etc.
+     * The config file must indicate the Data Source configuration name, host address,
+     * user, table name, change table name and the primary key. Otherwise, the check fails.
+     */
+    bool checkConfigValidity();
+
+    //Check if database contains the table.
+    bool checkTableExistence();
+    //Fetch the table schema and store into tableSchema
+    bool populateTableSchema(std::string & tableName);
+
+    /*
+     * Load the last accessed record RSID$ from the file.
+     * If the file does not exist(The first time create the indexes),
+     * it will query the database and fetch the latest record RSID$.
+     */
+    void loadLastAccessedLogRecordTime();
+
+    //Get the MAX RSID$ from Oracle database.
+    //Query : "SELECT MAX(RSID$) FROM changeTable";
+    void populateLastAccessedLogRecordTime();
+
+    //Allocate/Deallocate a SQL Statement handle
+    bool allocateSQLHandle(SQLHSTMT & hstmt);
+    void deallocateSQLHandle(SQLHSTMT & hstmt);
+};
+
+#endif /* ORACLECONNECTOR_H_ */
---
+++ test/wrapper/system_tests/adapter_oracle/adapter_oracle.py
@@ -0,0 +1,331 @@
+#These tests are used for the adapter_oracle
+#Require oracle connected, unixODBC and oracle driver installed.
+#Test 1: test loading records from the oracle table to create an index.
+#Test 2: When the server is running, update the record in oracle,
+#        then the listener should fetch the results.
+#Test 3: Shut down the engine, and delete the records in oracle.
+#        Then start the engine to test if the engine can fetch the changes.
+
+import sys, urllib2, json, time, subprocess, os, commands, signal,shutil
+
+sys.path.insert(0,'srch2lib')
+import test_lib
+
+import xml.etree.ElementTree as ET
+
+sys.path.append(os.getcwd()+'/../../../thirdparty/pyodbc/pyodbc/build/')
+
+try:
+    import pyodbc
+except ImportError:
+    os._exit(-1)
+
+
+
+port = '8087'
+serverHandle = None
+totalFailCount = 0
+binary_path = None
+dataSource = ''
+server = ''
+conn = None
+
+def populateUserPassFromXML(path):
+    global dataSource
+    global server
+    global dbName
+
+    tree = ET.parse(path)
+    dbKeyValues = list(tree.find('./dbParameters/dbKeyValues').getiterator('dbKeyValue'))
+    for i in dbKeyValues:
+        if(i.attrib['key']=='dataSource'):
+            dataSource = i.attrib['value']
+        if(i.attrib['key']=='server'):
+            server = i.attrib['value']
+
+#Start the SRCH2 engine with oracle config file.
+def startSrch2Engine():
+    global serverHandle
+    #Start the engine server
+    args = [binary_path , '--config-file=adapter_oracle/conf.xml']
+
+    if test_lib.confirmPortAvailable(port) == False:
+        print 'Port' + str(port) + ' already in use -aborting '
+        return -1
+
+    print 'starting engine: ' + args[0] + ' ' + args[1]
+    serverHandle = test_lib.startServer(args)
+    test_lib.pingServer(port)
+
+#Shut down the srch2 engine
+def shutdownSrch2Engine():
+    global serverHandle
+    #Shutdown the engine server
+    test_lib.killServer(serverHandle)
+
+#Compare the results with the expected outputs.
+def compareResults(testQueriesPath):
+    f_test = open(testQueriesPath,'r')
+    failCount = 0
+    global totalFailCount
+
+    for line in f_test:
+        #Get the query keyword and result from the input file
+        value = line.split('||')
+        queryValue = value[0].split()
+        resultValue = value[1].split()
+
+        #Construct the query
+        query = prepareQuery(queryValue)
+
+        #Execute the query
+        response = urllib2.urlopen(query).read()
+        response_json = json.loads(response)
+
+        #Check the result
+        failCount += checkResult(query, response_json['results'],resultValue)
+
+    totalFailCount += failCount
+
+#prepare the query based on the valid syntax
+def prepareQuery(queryKeywords):
+    query = 'http://localhost:' + port + '/search?'
+    # prepare the main query part
+    query = query + 'q='
+    # keywords section
+    for i in range(0, len(queryKeywords)):
+        if i == (len(queryKeywords)-1):
+            query=query+queryKeywords[i] # last keyword prefix
+        else:
+            query=query+queryKeywords[i]+'%20AND%20'
+
+    return query
+
+#Function of checking the results
+#Compare the record 'PROD_ID' part with the result value
+def checkResult(query, responseJson,resultValue):
+#    for key, value in responseJson:
+#        print key, value
+    isPass=1
+    if  len(responseJson) == len(resultValue):
+        for i in range(0, len(resultValue)):
+            #print response_json['results'][i]['record']['id']
+            if responseJson[i]['record']['PROD_ID'] !=  resultValue[i]:
+                isPass=0
+                print query+' test failed'
+                print 'query results||given results'
+                print 'number of results:'+str(len(responseJson))+'||'+str(len(resultValue))
+                for i in range(0, len(responseJson)):
+                    print responseJson[i]['record']['PROD_ID']+'||'+resultValue[i]
+                break
+    else:
+        isPass=0
+        print query+' test failed'
+        print 'query results||given results'
+        print 'number of results:'+str(len(responseJson))+'||'+str(len(resultValue))
+        maxLen = max(len(responseJson),len(resultValue))
+        for i in range(0, maxLen):
+            if i >= len(resultValue):
+             print responseJson[i]['record']['PROD_ID']+'||'
+            elif i >= len(responseJson):
+             print '  '+'||'+resultValue[i]
+            else:
+             print responseJson[i]['record']['PROD_ID']+'||'+resultValue[i]
+
+    if isPass == 1:
+        print  query+' test pass'
+        return 0
+    return 1
+
+
+#Test 1: test loading index from the oracle table to create the index.
+def testCreateIndexes(conn,sqlQueriesPath,testQueriesPath):
+    #Create the test table and Insert record into it
+    f_sql = open(sqlQueriesPath,'r')
+    for line in f_sql:
+        conn.cursor().execute(line)
+        print line
+    conn.commit()
+
+    #Start the engine and wait to fetch the data, 
+    #the engine will create an index from the oracle table
+    startSrch2Engine()
+    time.sleep(5)
+
+    #Compare the results with the expected results
+    compareResults(testQueriesPath)
+    print '=============================='
+
+#Test 2: When the server is running, update the record in oracle, 
+#then the listener should fetch the results.
+def testRunListener(conn,sqlQueriesPath,testQueriesPath):
+    #Modify the table while the srch2 engine is running.
+    f_sql = open(sqlQueriesPath,'r')
+    for line in f_sql:
+        conn.cursor().execute(line)
+        print line
+    conn.commit()
+
+    #Wait for the engine to fetch the changes
+    time.sleep(5)
+
+    #Compare the results with the expected results
+    compareResults(testQueriesPath)
+    print '=============================='
+
+#Test 3: Shut down the engine, and delete the records in oracle.
+#Then start the engine to test if the engine can fetch the changes
+def testOfflineLog(conn,sqlQueriesPath,testQueriesPath):
+    #Shutdown the engine
+    shutdownSrch2Engine()
+    time.sleep(3)
+
+    #Modify the table while the srch2 engine is not running
+    f_sql = open(sqlQueriesPath,'r')
+    for line in f_sql:
+        conn.cursor().execute(line)
+        print line
+    conn.commit()
+
+    #Start the engine and wait it fetch the changes,
+    #the engine will get the offline changes.
+    startSrch2Engine()
+    time.sleep(4)
+
+    #Compare the results with the expected results
+    compareResults(testQueriesPath)
+
+    #Shutdown the engine. Finish the test.
+    shutdownSrch2Engine()
+    print '=============================='
+
+def signal_handler(signum,frame):
+    return
+def connectToDB(myUserName, myPassword):
+    global conn
+    global dataSource
+    global server
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(3)
+    connstr = 'DRIVER={'+str(dataSource)+'};SERVER='+str(server)+';UID='+str(myUserName)+';PWD='+str(myPassword)+';'
+
+    try:
+        conn = pyodbc.connect(connstr)
+    except Exception, exc: 
+        print 'Access denied while connecting to the Oracle database. Make sure the Oracle username, password, data source, server are correct in the ./adapter_oracle/conf.xml '
+        print 'Also, please run the command in the ./adapter_oracle/readme.txt'
+        print exc
+        os._exit(-1)
+    finally:
+        signal.alarm(0)
+
+if __name__ == '__main__':
+    if(os.path.exists("data")):
+        shutil.rmtree("data")
+
+    populateUserPassFromXML('./adapter_oracle/conf.xml')
+
+    connectToDB('cdcpub','cdcpub')
+        
+    #Remove the table
+    conn.cursor().execute("""BEGIN
+                             EXECUTE IMMEDIATE 'DROP USER CDCSUB';
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                          """)
+
+    conn.cursor().execute("""BEGIN
+                             BEGIN
+                             DBMS_CDC_PUBLISH.DROP_CHANGE_TABLE('cdcpub','products_ct','Y');
+                             END;
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                         """)
+
+    conn.cursor().execute("""BEGIN
+                             BEGIN
+                             DBMS_CDC_PUBLISH.DROP_CHANGE_SET('cdcset');
+                             END;
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                          """)
+
+    conn.cursor().execute("""BEGIN
+                             EXECUTE IMMEDIATE 'DROP TABLE CDCPUB.PRODUCTS';
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                          """)
+
+    #Create change table and change table set
+    conn.cursor().execute("CREATE USER cdcsub IDENTIFIED BY cdcsub DEFAULT TABLESPACE ts_cdcpub;")
+    conn.cursor().execute("GRANT CREATE TABLE TO cdcsub;")
+    conn.cursor().execute("GRANT CREATE SESSION TO cdcsub;")
+    conn.cursor().execute("GRANT CREATE VIEW TO cdcsub;")
+    conn.cursor().execute("GRANT UNLIMITED TABLESPACE TO cdcsub;")
+    conn.cursor().execute("CREATE TABLE cdcpub.products(PROD_ID NUMBER(6),PROD_NAME VARCHAR2(50),PROD_LIST_PRICE NUMBER(8,2));")
+    conn.cursor().execute("""BEGIN
+                             DBMS_CDC_PUBLISH.CREATE_CHANGE_SET(change_set_name => 'cdcset',description => 'Change set for srch2 test',change_source_name => 'SYNC_SOURCE');
+                             END;
+                          """)
+    conn.cursor().execute("""BEGIN
+                             DBMS_CDC_PUBLISH.CREATE_CHANGE_TABLE(owner => 'cdcpub',change_table_name => 'products_ct',change_set_name => 'cdcset',source_schema => 'cdcpub',source_table => 'PRODUCTS',column_type_list => 'PROD_ID NUMBER (6),PROD_NAME VARCHAR2 (50),PROD_LIST_PRICE NUMBER (8,2) ',capture_values => 'both',rs_id => 'y',row_id => 'n',user_id => 'n',timestamp => 'n',object_id => 'n',source_colmap => 'y',target_colmap => 'y',DDL_MARKERS=>'n', options_string => 'TABLESPACE ts_cdcpub');
+                             END;
+                          """)
+    conn.cursor().execute("GRANT ALL ON cdcpub.products TO cdcsub;")
+    conn.cursor().execute("GRANT ALL ON cdcpub.products_ct TO cdcsub;")
+
+    conn.commit()
+
+    #Start the test cases
+    binary_path = sys.argv[1]
+    testCreateIndexes(conn,sys.argv[2],sys.argv[3])
+    testRunListener(conn,sys.argv[4],sys.argv[5])
+    testOfflineLog(conn,sys.argv[6],sys.argv[7])
+
+    print '=============================='
+    shutdownSrch2Engine()
+    time.sleep(3)
+
+    #Remove the table
+    conn.cursor().execute("""BEGIN
+                             EXECUTE IMMEDIATE 'drop user cdcsub';
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                          """)
+
+    conn.cursor().execute("""BEGIN
+                             BEGIN
+                             DBMS_CDC_PUBLISH.DROP_CHANGE_TABLE('cdcpub','products_ct','Y');
+                             END;
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                         """)
+
+    conn.cursor().execute("""BEGIN
+                             BEGIN
+                             DBMS_CDC_PUBLISH.DROP_CHANGE_SET('cdcset');
+                             END;
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                          """)
+
+    conn.cursor().execute("""BEGIN
+                             EXECUTE IMMEDIATE 'DROP TABLE CDCPUB.PRODUCTS';
+                             EXCEPTION
+                             WHEN OTHERS THEN NULL;
+                             END;
+                          """)
+
+    conn.close()
+    if(os.path.exists("data")):
+        shutil.rmtree("data")
+    
+    os._exit(totalFailCount)
